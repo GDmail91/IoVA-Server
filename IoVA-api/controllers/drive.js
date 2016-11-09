334ebc2226d3fe086c6e480d00f97e27a339381a
GDmail <GDmail91@gmail.com>
2016-11-09 21:35:40 +0900

    Fix error process

--- IoVA-api/controllers/drive.js
+++ IoVA-api/controllers/drive.js
@@ -18,30 +18,30 @@ module.exports = {
         };
 
         var drive_datas = JSON.parse(data.drive_raw_data);
-        var error_index = [];
+        var count = 0;
         drive_datas.forEach(function(drive_data, index) {
             driveInfoModel.insert(data.access_token, drive_data)
-                .catch(function(result) {
-                    error.push(error_index);
+                .then(function(result) {
+                    if (++count < drive_datas.length) {
+                        res.statusCode = 200;
+                        return res.json({
+                            msg: "Completed insert",
+                            data: {
+                                insert_length: drive_datas.length
+                            }
+                        });
+                    }
+                })
+                .catch(function(err) {
+                    res.statusCode = 500;
+                    return res.json({
+                        msg: "Error on insert data. Please check your data",
+                        data: {
+                            error_index: index
+                        }
+                    });
                 });
         });
 
-        if (error_index.length > 0) {
-            res.statusCode = 500;
-            return res.json({
-                msg: "Error on insert data. Please check index",
-                data: {
-                    error_index: error_index
-                }
-            });
-        }
-
-        res.statusCode = 200;
-        return res.json({
-            msg: "Completed insert",
-            data: {
-                insert_length: drive_datas.length
-            }
-        });
     }
 };
