cc6607006c3056571e64b943a9df8fa1b4edd15d
GDmail <GDmail91@gmail.com>
2016-11-09 22:15:19 +0900

    Fix mistake / Add safeScoreModel

--- IoVA-api/controllers/drive.js
+++ IoVA-api/controllers/drive.js
@@ -2,6 +2,7 @@
  * Created by YS on 2016-11-09.
  */
 var driveInfoModel = require('../models/driveInfoModel');
+var safeScoreModel = require('../models/safeScoreModel');
 
 /**
  * @type {{
@@ -14,22 +15,19 @@ module.exports = {
     postDriveInfo: function (req, res, next) {
         var data = {
             access_token: req.header('access-token'),
-            drive_raw_data: req.body.drive_raw_data
+            raw_drive_data: req.body.raw_drive_data,
+            raw_safe_score: req.body.raw_safe_score
         };
 
-        var drive_datas = JSON.parse(data.drive_raw_data);
+        var drive_datas = JSON.parse(data.raw_drive_data);
+        var safe_score = JSON.parse(data.raw_safe_score);
         var count = 0;
+
         drive_datas.forEach(function(drive_data, index) {
             driveInfoModel.insert(data.access_token, drive_data)
                 .then(function(result) {
-                    if (++count < drive_datas.length) {
-                        res.statusCode = 200;
-                        return res.json({
-                            msg: "Completed insert",
-                            data: {
-                                insert_length: drive_datas.length
-                            }
-                        });
+                    if (++count == drive_datas.length) {
+                        safeScoreInsert();
                     }
                 })
                 .catch(function(err) {
@@ -43,5 +41,18 @@ module.exports = {
                 });
         });
 
+        function safeScoreInsert() {
+            safeScoreModel.insert(data.access_token, safe_score)
+                .then(function(result) {
+                    res.statusCode = 200;
+                    return res.json({
+                        msg: "Completed insert",
+                        data: {
+                            insert_length: drive_datas.length
+                        }
+                    });
+                })
+                .catch(next);
+        }
     }
 };
---
+++ IoVA-api/models/safeScoreModel.js
@@ -0,0 +1,59 @@
+/**
+ * Created by YS on 2016-11-09.
+ */
+var credentials = require('../../credentials');
+var mysqlSetting = require('./mysqlSetting');
+
+var driveInfoModel = {
+    insert : function(access_token, safe_score) {
+        return new Promise(function(resolved, rejected) {
+            mysqlSetting.getPool()
+                .then(mysqlSetting.getConnection)
+                .then(mysqlSetting.connBeginTransaction)
+                .then(function(context) {
+                    return new Promise(function(resolved, rejected) {
+                        var insert = [access_token,
+                            safe_score.drive_id,
+                            safe_score.safe_distance_count,
+                            safe_score.speeding_count,
+                            safe_score.fast_acc_count,
+                            safe_score.fast_break_count,
+                            safe_score.sudden_start_count,
+                            safe_score.sudden_stop_count,
+                            safe_score.drive_start,
+                            safe_score.drive_stop];
+                        var sql = "INSERT INTO safe_score SET " +
+                            "score_user_id = (SELECT user_id FROM users WHERE access_token = ?), " +
+                            "score_drive_id = ?," +
+                            "safe_distance_count = ?," +
+                            "speeding_count = ?," +
+                            "fast_acc_count = ?," +
+                            "fast_break_count = ?," +
+                            "sudden_start_count = ?," +
+                            "sudden_stop_count = ?," +
+                            "drive_start = ?," +
+                            "drive_stop = ? ";
+                        context.connection.query(sql, insert, function (err, rows) {
+                            if (err) {
+                                var error = new Error("Failed insert information");
+                                error.status = 500;
+                                console.error(err);
+                                return rejected(error);
+                            }
+
+                            return resolved(context);
+                        });
+                    });
+                })
+                .then(mysqlSetting.commitTransaction)
+                .then(function(data) {
+                    return resolved(data);
+                })
+                .catch(function(err) {
+                    return rejected(err);
+                });
+        });
+    }
+};
+
+module.exports = driveInfoModel;
