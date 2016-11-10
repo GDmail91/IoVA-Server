5cae36246f0f3784805ba1257c1ec78664c3a378
GDmail <GDmail91@gmail.com>
2016-11-10 17:00:28 +0900

    Change upload way to file upload

--- IoVA-api/controllers/drive.js
+++ IoVA-api/controllers/drive.js
@@ -14,46 +14,102 @@ module.exports = {
 
     postDriveInfo: function (req, res, next) {
         var data = {
-            access_token: req.header('access-token'),
-            raw_drive_data: req.body.raw_drive_data,
-            raw_safe_score: req.body.raw_safe_score
+            access_token: req.header('access-token')
         };
 
-        var drive_datas = JSON.parse(data.raw_drive_data);
-        var safe_score = JSON.parse(data.raw_safe_score);
-        var count = 0;
+        console.log(data);
 
-        drive_datas.forEach(function(drive_data, index) {
-            driveInfoModel.insert(data.access_token, drive_data)
-                .then(function(result) {
-                    if (++count == drive_datas.length) {
-                        safeScoreInsert();
-                    }
-                })
-                .catch(function(err) {
-                    res.statusCode = 500;
-                    return res.json({
-                        msg: "Error on insert data. Please check your data",
-                        data: {
-                            error_index: index
+        var drive_datas;
+        var safe_score;
+
+        var formidable = require('formidable');
+        var form = new formidable.IncomingForm();
+
+        // 파일 폼 파싱 ##
+        form.parse(req, function(err, fields, files) {
+            if(err) return next();
+
+            (function() {
+                return new Promise(function(resolved, rejected) {
+                    var fs = require('fs');
+                    var driveBuffer = new Buffer(0);
+
+                    // Read in the file, convert it to base64
+                    var driveFileStream = fs.createReadStream(files.raw_drive_data.path);
+
+                    driveFileStream.on('error', function (err) {
+                        if (err) {
+                            throw err;
                         }
+                    });
+                    driveFileStream.on('data', function (data) {
+                        driveBuffer = Buffer.concat([driveBuffer, data]);
+                    });
+                    driveFileStream.on('end', function() {
+                        drive_datas = JSON.parse(driveBuffer);
+                        return resolved();
                     });
                 });
-        });
+            })()
+                .then(function() {
+                    return new Promise(function(resolved, rejected) {
+                        var fs = require('fs');
+                        var scoreBuffer = new Buffer(0);
 
-        function safeScoreInsert() {
-            safeScoreModel.insert(data.access_token, safe_score)
-                .then(function(result) {
-                    res.statusCode = 200;
-                    return res.json({
-                        msg: "Completed insert",
-                        data: {
-                            insert_length: drive_datas.length
-                        }
+                        // Read in the file, convert it to base64
+                        var scoreFileStream = fs.createReadStream(files.raw_safe_score.path);
+
+                        scoreFileStream.on('error', function (err) {
+                            if (err) {
+                                throw err;
+                            }
+                        });
+                        scoreFileStream.on('data', function (data) {
+                            scoreBuffer = Buffer.concat([scoreBuffer, data]);
+                        });
+                        scoreFileStream.on('end', function () {
+                            safe_score = JSON.parse(scoreBuffer);
+                            resolved();
+                        });
                     });
                 })
-                .catch(next);
-        }
+                .then(function() {
+                    return new Promise(function(resolved, rejected) {
+                        var count = 0;
+                        drive_datas.forEach(function (drive_data, index) {
+                            driveInfoModel.insert(data.access_token, drive_data)
+                                .then(function (result) {
+                                    if (++count == drive_datas.length) {
+                                        resolved();
+                                    }
+                                })
+                                .catch(function (err) {
+                                    res.statusCode = 500;
+                                    return res.json({
+                                        msg: "Error on insert data. Please check your data",
+                                        data: {
+                                            error_index: index
+                                        }
+                                    });
+                                });
+                        });
+                    });
+                })
+                .then(function() {
+                    safeScoreModel.insert(data.access_token, safe_score)
+                        .then(function(result) {
+                            res.statusCode = 200;
+                            return res.json({
+                                msg: "Completed insert",
+                                data: {
+                                    insert_length: drive_datas.length
+                                }
+                            });
+                        })
+                        .catch(next);
+                })
+                .catch(next)
+        });
     },
 
     getLastIndex: function(req, res, next) {
